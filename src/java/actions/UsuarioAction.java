3c64711393111b14a2fe1f6add3b9f3cb4e2f809
Luis <sednanref2004@gmail.com>
2013-09-03 12:00:00 -0430

    quasi sesiones

--- src/java/actions/UsuarioAction.java
+++ src/java/actions/UsuarioAction.java
@@ -4,13 +4,16 @@
  */
 package actions;
 
+import static com.opensymphony.xwork2.Action.SUCCESS;
 import com.opensymphony.xwork2.ActionSupport;
+import java.util.Map;
+import org.apache.struts2.interceptor.SessionAware;
 
 /**
  *
  * @author CHANGE Gate
  */
-public class UsuarioAction extends ActionSupport {
+public class UsuarioAction extends ActionSupport implements SessionAware {
     
     private String usbid;
     private int cedula;
@@ -18,6 +21,49 @@ public class UsuarioAction extends ActionSupport {
     private String apellido;
     private String contrasena;
     private String rol;
+    private Map session;
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String execute() {
+        return SUCCESS;
+    }
+    
+    /**
+     *
+     */
+    @Override
+    public void validate() {
+        if (getUsbid().length() == 0) {
+            addFieldError("userName", getText("username.required"));
+        } else if (!getUsbid().equals("Shiva")) {
+            addFieldError("userName", getText("username.wrong"));
+        }
+        if (getContrasena().length() == 0) {
+            addFieldError("password", getText("password.required"));
+        }
+        session.put("usbid", getUsbid());
+        session.put("constrasena", getContrasena());
+    }
+    
+    /**
+     *
+     * @return
+     */
+    public Map getSession() {
+        return session;
+    }
+
+    /**
+     *
+     * @param session
+     */
+    public void setSession(Map session) {
+        this.session = session;
+    }
 
     /**
      *
@@ -114,4 +160,5 @@ public class UsuarioAction extends ActionSupport {
     public void setRol(String rol) {
         this.rol = rol;
     }
+
 }
